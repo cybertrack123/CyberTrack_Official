659d42bdf1de393963d5077790309e5319c3d8a0
cybertrack123 <79833407+cybertrack123@users.noreply.github.com>
2021-03-03 17:26:27 +0530

    Update urls.py

--- CyberTrack/urls.py
+++ CyberTrack/urls.py
@@ -23,6 +23,7 @@ from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('Cyber/', include('Cyber.urls')),
+    path('Cyber/views/', views.Index, name='Index')
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
