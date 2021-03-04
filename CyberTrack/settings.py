1c922e6f92a8bf8fcd528e121488ed745777f355
cybertrack123 <79833407+cybertrack123@users.noreply.github.com>
2021-03-04 17:06:32 +0530

    Update settings.py

--- CyberTrack/settings.py
+++ CyberTrack/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7$(!y^kw640yuxpsa%h+37stnfvp)ndwuyx#9)u30i6d!^!wk6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['http://127.0.0.1:8000/','https://cybertrack123.herokuapp.com']
+ALLOWED_HOSTS = ['cybertrack123.herokuapp.com']
 
 
 # Application definition
